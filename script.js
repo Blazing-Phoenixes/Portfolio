009ba74cbce6a9e0010e69748b7acb18a0d9d849
Blazing-Phoenixes <bharathkumar16227@gmail.com>
2025-07-21 03:01:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.getElementById("show1").addEventListener("click", function () {
   content.classList.toggle("hidden");
   var content = document.getElementById("content4");
   content.classList.toggle("hidden");
+  var content = document.getElementById("content5");
+  content.classList.toggle("hidden");
 }); //project of py content
 
 document.getElementById("show2").addEventListener("click", function () {
